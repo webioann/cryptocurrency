80f5bd596718723aaa0a9ba5feb3361bf557404d
webioann <ioannn7777@gmail.com>
2022-06-14 16:46:38 +0300

    get coins data from CoinGecko API with axios

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React,{ useState,useEffect } from 'react'
+import axios from "axios"
 
-const HomePage = () => {
-    
+const HomePage:React.FC = () => {
+
+    const [coins_data,setCoinsData] =useState<object[]>([])
+
+    const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+
+    useEffect(() => {
+        axios.get(url)
+        .then( respons => console.log(respons.data))
+    },[url])
+
     return (
         <div>
             Home Page
