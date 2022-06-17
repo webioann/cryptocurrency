6c3f52bbd5d64342c86517f52b334a25809bb452
webioann <ioannn7777@gmail.com>
2022-06-17 14:53:25 +0300

    finaly complite make type empty object in useState

--- src/TYPES.ts
+++ src/TYPES.ts
@@ -3,11 +3,11 @@ export type PropsChildrenType = {
     children: JSX.Element | JSX.Element[]
 }
 // ===== SparkLine ==========
-export interface SparkLineType {
+export interface SparkLinePriceType {
     price: Array<number>
 }
-export interface SparkType {
-    sparkline_in_7d: SparkLineType;
+export interface SparkLineType {
+    sparkline_in_7d: SparkLinePriceType;
 }
 // ====== coins ===========
 export interface CoinsType {
@@ -46,5 +46,44 @@ export type TrendingItem = {
 export interface TrendingType {
     item: TrendingItem;
 }
-// ===========================
-
+// ========= unit_coin ==============
+type descEngType = {
+    eng: string
+}
+type img = {
+    large: string;
+    small: string;
+    thumb: string;
+}
+type market_data = {
+    current_price: object;
+    sparkline_in_7d: SparkLineType;
+    market_cap: object;
+    total_volume: object;
+    high_24h: object;
+    low_24h: object;
+    price_change_percentage_24h: number;
+    price_change_percentage_7d: number;
+    price_change_percentage_14d: number;
+    price_change_percentage_30d: number;
+    price_change_percentage_60d: number;
+    price_change_percentage_1y: number;
+}
+type ticker = {
+    base: string;
+    is_anomaly: boolean;
+    market: object;
+    target: string;
+}
+export type UnitCoinType = {
+    id: string;
+    symbol: string;
+    name: string;
+    image: img;
+    market_data: market_data;
+    market_cap_rank: number;
+    hashing_algorithm: string;
+    tickers: Array<ticker>;
+    liquidity_score: number;
+    description: descEngType;
+}
--- src/components/CoinPage/CoinPage.tsx
+++ src/components/CoinPage/CoinPage.tsx
@@ -1,12 +1,28 @@
 import React,{ useState,useEffect } from 'react'
+import { useAppDispatch,useAppSelector } from '../../Redux/store'
+import { getFetchCoins } from '../../Redux/reduxSlice'
+import axios from "axios"
+import { UnitCoinType } from '../../TYPES'
+
 import './coin-page.scss'
-import PageContainer from '../PageContainer/PageContainer'
 
 const CoinPage:React.FC = () => {
 
+    const [coin,setUnitCoin] = useState<UnitCoinType>( {} as UnitCoinType )
+    const coin_url = "https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&sparkline=true"
+    
+    useEffect(() => {
+        axios.get(coin_url)
+        .then( respons => {
+            setUnitCoin(respons.data)
+            console.log(respons.data);
+    })
+    },[coin_url])
+
     return (
         <div className='g-page-container'>
-            <h1>COIN PAGE</h1>        
+            <h1>COIN PAGE = {coin.name}</h1>
+            <img src={coin.image?.small} alt='/' />      
         </div>
     )
 }
