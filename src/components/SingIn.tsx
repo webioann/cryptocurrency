8d4c6498e46b27ebcaa3e74881cf1ddeea50738e
webioann <ioannn7777@gmail.com>
2022-06-20 00:43:52 +0300

    created method opening hidden password

--- src/components/SingIn.tsx
+++ src/components/SingIn.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import { Link } from 'react-router-dom'
-import { AiFillLock, AiOutlineMail } from 'react-icons/ai'
+import { AiFillLock, AiOutlineMail,AiFillUnlock } from 'react-icons/ai'
+
+import { GoEye,GoEyeClosed } from 'react-icons/go'
 import '../CSS/sing-in.scss'
 
 const SingIn:React.FC = () => {
     
+    const [lock,setLock] = useState<string>('password')
+
+    const showPassword = () => {
+        if( lock === 'password' ) {
+            setLock('text')
+        }
+        else{
+            setLock('password')
+        }
+    }
+
     return (
         <div className='g-page-container'>
             <div className='sing-in-wrapper'>
@@ -20,11 +33,14 @@ const SingIn:React.FC = () => {
                     <div className='password-box'>
                         <label>Password</label>
                         <div className='password-input'>
-                            <input type='password' placeholder='password'/>
-                            <AiFillLock className='input-icon'/>
+                            <input type={lock} placeholder='password'/>
+                            {lock === 'text' 
+                                ? <GoEye className='input-icon' onClick={showPassword}/> 
+                                : <GoEyeClosed className='input-icon' onClick={showPassword}/>
+                            }
                         </div>
                     </div>
-                    <button className='btn'>Sing in</button>
+                    <button className='btn'>Sing In</button>
                 </form>
                 <div className='question'>
                     <p className='q-text'>Don't have an account ?</p>
--- src/components/SingUp.tsx
+++ src/components/SingUp.tsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { AiFillLock, AiOutlineMail,AiFillUnlock } from 'react-icons/ai'
+import { GoEye,GoEyeClosed } from 'react-icons/go'
 import '../CSS/sing-up.scss'
 
 const SingUp = () => {
+
+    const [lock,setLock] = useState<string>('password')
+
+    const showPassword = () => {
+        if( lock === 'password' ) {
+            setLock('text')
+        }
+        else{
+            setLock('password')
+        }
+    }
     
     return (
         <div className='g-page-container'>
-            <p>SingUp</p>
+            <div className='sing-in-wrapper'>
+                <h1 className='header'>Sing Up</h1>
+                <form>
+                    <div className='email-box'>
+                        <label>Email</label>
+                        <div className='email-input'>
+                            <input type='email' placeholder='email'/>
+                            <AiOutlineMail className='input-icon'/>
+                        </div>
+                    </div>
+                    <div className='password-box'>
+                        <label>Password</label>
+                        <div className='password-input'>
+                            <input type={lock} placeholder='password'/>
+                            {lock === 'text' 
+                                ? <GoEye className='input-icon' onClick={showPassword}/> 
+                                : <GoEyeClosed className='input-icon' onClick={showPassword}/>
+                            }
+                        </div>
+                    </div>
+                    <button className='btn'>Sing Up</button>
+                </form>
+                <div className='question'>
+                    <p className='q-text'>Already have an account ?</p>
+                    <Link to='/singin' className='q-link'>Sing In</Link>
+                </div>
+            </div>
         </div>
     )
 }
