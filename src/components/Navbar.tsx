594194a86ecad202e43349dc0c0c7b862eeb713a
webioann <ioannn7777@gmail.com>
2022-06-24 12:33:18 +0300

    crete button sign-out in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,9 @@
 import React,{ useState } from 'react'
-import { useAppSelector } from '../Redux/store'
-import { Link } from "react-router-dom"
+import { useAppSelector, useAppDispatch } from '../Redux/store'
+import { removeUser } from '../Redux/reduxSlice'
+import { Link,useNavigate } from 'react-router-dom'
+import { getAuth, signOut } from "firebase/auth"
+
 import ThemeToggle from './ThemeToggle'
 import BurgerSpiner from './BurgerSpiner'
 import { FaRegUser } from 'react-icons/fa'
@@ -12,6 +15,19 @@ const Navbar:React.FC = () => {
     const theme = useAppSelector(state => state.redux.theme_mode)
     const user = useAppSelector(state => state.redux.user)
     const [active,setActive] = useState<boolean>(false)
+    const dispatch = useAppDispatch()
+    const navigate = useNavigate()
+    const auth = getAuth()
+
+    const User_Sign_Out = () => {
+        signOut(auth).then(() => {
+            dispatch(removeUser())
+            navigate("/")
+        }).catch((error) => {
+            console.log(error);
+        });    
+    }
+
 
     return (
         <nav className={`navbar ${theme}-nav`}>
@@ -22,13 +38,16 @@ const Navbar:React.FC = () => {
             </div>
             <div className={active ? 'menu active' : 'menu'}>
                 { user ? (
-                    <Link to="/account" className={`link user ${theme}-nl`}>
-                        <div className='img-wrapper'>
-                            {/* <img src='' alt='#'/> */}
-                            <FaRegUser/>
-                        </div>
-                        <span className='user-name'>{user}</span>
-                    </Link>
+                    <>
+                        <Link to="/account" className={`link user ${theme}-nl`}>
+                            <div className='img-wrapper'>
+                                {/* <img src='' alt='#'/> */}
+                                <FaRegUser/>
+                            </div>
+                            <span className='user-name'>{user}</span>
+                        </Link>
+                        <div onClick={User_Sign_Out} className={`link ${theme}-sign-out`}>Sing Out</div>
+                    </>
                     ) : (
                     <>
                         <Link to="/signin" className={`link ${theme}-nl`}>Sing In</Link>
