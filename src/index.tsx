69e397a734fd74d0705741bd8260c3666eff5c26
webioann <ioannn7777@gmail.com>
2022-06-26 14:37:00 +0300

    the console was cleared of warnings

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import App from './components/App'
 import store from "./Redux/store"
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import './Firebase/firebase-config'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
     <Provider store={store}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
-    </Provider>,
-    document.getElementById('root')
+    </Provider>
 );
