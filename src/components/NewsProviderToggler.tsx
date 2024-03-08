7e8d2aa1f1667a412e220e7bf7920066dbedea71
Webioann <ioannn7777@gmail.com>
2024-03-08 10:58:39 +0200

    active class for current news provider button

--- src/components/NewsProviderToggler.tsx
+++ src/components/NewsProviderToggler.tsx
@@ -1,20 +1,45 @@
 import React from 'react';
 import { changeNewsProvider } from '../Redux/reduxSlice';
-import { useAppDispatch } from '../Redux/store'
+import { useAppDispatch, useAppSelector } from '../Redux/store'
 import '../CSS/news-provider.scss'
 
 function NewsProviderToggler() {
 
     const dispatch = useAppDispatch()
+    const newsProvider = useAppSelector(state => state.redux.newsProvider)
 
     return (
         <div className='provider-list'>
-            <span className='provider' onClick={() => dispatch(changeNewsProvider('coindesk'))}>Coindesk</span>
-            <span className='provider' onClick={() => dispatch(changeNewsProvider('cointelegraph'))}>Cointelegraph</span>
-            <span className='provider' onClick={() => dispatch(changeNewsProvider('bitcoinist'))}>Bitcoinist</span>
-            <span className='provider' onClick={() => dispatch(changeNewsProvider('decrypt'))}>Decrypt</span>
-            <span className='provider' onClick={() => dispatch(changeNewsProvider('bsc'))}>BSC News</span>
-            <span className='provider' onClick={() => dispatch(changeNewsProvider('theguardian'))}>The guardian</span>
+            <span 
+                className={newsProvider === 'coindesk' ? 'provider active-provider' : 'provider'} 
+                onClick={() => dispatch(changeNewsProvider('coindesk'))}>
+                Coindesk
+            </span>
+            <span 
+                className={newsProvider === 'cointelegraph' ? 'provider active-provider' : 'provider'} 
+                onClick={() => dispatch(changeNewsProvider('cointelegraph'))}>
+                Cointelefraph
+            </span>
+            <span 
+                className={newsProvider === 'bitcoinist' ? 'provider active-provider' : 'provider'} 
+                onClick={() => dispatch(changeNewsProvider('bitcoinist'))}>
+                Bitcoinist
+            </span>
+            <span 
+                className={newsProvider === 'decrypt' ? 'provider active-provider' : 'provider'} 
+                onClick={() => dispatch(changeNewsProvider('decrypt'))}>
+                Decrypt
+            </span>
+            <span 
+                className={newsProvider === 'bsc' ? 'provider active-provider' : 'provider'} 
+                onClick={() => dispatch(changeNewsProvider('bsc'))}>
+                BSC News
+            </span>
+            <span 
+                className={newsProvider === 'theguardian' ? 'provider active-provider' : 'provider'} 
+                onClick={() => dispatch(changeNewsProvider('theguardian'))}>
+                The guardian
+            </span>
         </div>
     )
 }
