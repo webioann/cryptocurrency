8d8695eac3d2ff4cb1f8b28efa37ce17201baa23
webioann <webioann@gmail.com>
2025-08-10 13:11:06 +0300

    created nice gitignore file

--- src/components/LineChart.tsx
+++ src/components/LineChart.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, Suspense } from 'react'
 import { Line } from 'react-chartjs-2'
 import { useOptionsForChart } from '../hooks/useOptionsForChart'
 import { Chart as ChartJS,CategoryScale,LinearScale,PointElement,LineElement,Title,Tooltip,Legend,Filler } from 'chart.js'
